6eac0227d76402ad1f4e9e09b01add46fcb05dbe
Jeremy Wells <jeremysheridanwells@Jeremys-MacBook-Pro.local>
2017-12-06 10:31:01 -0600

    displaying presentation data in view

--- db/migrate/20171205175359_create_presentations.rb
+++ db/migrate/20171205175359_create_presentations.rb
@@ -1,7 +1,7 @@
 class CreatePresentations < ActiveRecord::Migration[5.1]
   def change
     create_table :presentations do |t|
-      t.integer :user_id
+      t.integer :user_id,
       t.string :title
 
       t.timestamps
