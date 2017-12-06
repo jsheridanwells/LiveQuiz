60dc2f2b70b8562b6cc509ef897009695ee42265
Jeremy Wells <jeremysheridanwells@Jeremys-MacBook-Pro.local>
2017-12-06 09:54:35 -0600

    seeds working

--- app/controllers/presentations_controller.rb
+++ app/controllers/presentations_controller.rb
@@ -2,7 +2,9 @@ class PresentationsController < ApplicationController
   before_action :authenticate
 
   def index
-
+    @presentations = Presentation.where(user_id: current_user.id)
   end
+
+
 
 end
